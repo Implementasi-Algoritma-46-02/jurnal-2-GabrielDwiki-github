136d2b62d798b9716f0e371c7af916286991cc6a
codingku <rp755404@gmail.com>
2024-09-24 20:40:47 +0700

    coding hello world

--- Jurnal1.java
+++ Jurnal1.java
@@ -3,7 +3,7 @@ public class Jurnal1 {
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
 
-        System.out.print("Hello ");
-        System.out.print("world ");
+        System.out.print("Hello world!!");
+        
     }
 }
