c416ebfadc8864151fe3f017dc05d98c35c1e525
codingku <rp755404@gmail.com>
2024-09-24 20:50:49 +0700

    codingku

--- Jurnal1.java
+++ Jurnal1.java
@@ -3,7 +3,7 @@ public class Jurnal1 {
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
 
-        System.out.print("Hello world!!");
+        System.out.print("Hello World!!");
         
     }
 }
